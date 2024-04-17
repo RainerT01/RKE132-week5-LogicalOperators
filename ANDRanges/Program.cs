b8eff214930a326b2d1ee5e8ce6eaa7710e1784c
Rainer Targama <rainertargama@gmail.com>
2024-04-17 14:03:37 +0300

    week5

---
+++ ANDRanges/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter the temperature in Celsius:");
+int temp = Int32.Parse(Console.ReadLine());
+if (temp >= 30)
+{
+    Console.WriteLine("Boling (haha) hot.");
+}
+else if (temp < 30 && temp >= 20)
+{
+    Console.WriteLine("Hot.");
+}
+else if (temp < 20 && temp >= 14)
+{
+    Console.WriteLine("Warm.");
+}
+else if (temp < 15 && temp >= 10)
+{
+    Console.WriteLine("Chilly.");
+}
+else if (temp < 10 && temp >= 0)
+{
+    Console.WriteLine("Cold.");
+}
+else
+{
+    Console.WriteLine("Freezing.");
+}
